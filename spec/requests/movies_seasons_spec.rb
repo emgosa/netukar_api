da9ed1f6a6feb7f81075e00dc97ca3afbd4a459b
emilio <e_g_salas_servext_rsi@cajarural.com>
2021-03-21 16:55:36 +0100

    added rspec for movies seasons

--- spec/requests/movies_seasons_spec.rb
+++ spec/requests/movies_seasons_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "MoviesSeasons", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:movies) { create_list(:movie, 3) }
+  let!(:seasons) { create_list(:season, 3, number: 1) }
+
+  describe "GET /movies_seasons_ordered_by_created_at" do
+    before { get '/movies_seasons_ordered_by_created_at' }
+
+    it 'returns movies and seasons' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(6)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 end
