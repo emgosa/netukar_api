522484d903ee443cdad47ca852c03f512190b088
emilio <e_g_salas_servext_rsi@cajarural.com>
2021-03-21 16:52:32 +0100

    fixed purchases

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,7 +2,14 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :purchasable, polymorphic: true, optional: true
   validates_presence_of :quality, :price, :purchasable_id, :purchasable_type, :user_id, :begin_at, :end_at
-  validates :user_id, uniqueness: {scope: [:purchasable_id, :purchasable_type]}
+  validates :user_id, uniqueness: {scope: [:user_id, :purchasable_id, :purchasable_type]}, unless: :already_available
 
-  scope :alive, -> { where("begin_at < :time and end_at > :time", time: DateTime.now) }
+  def alive
+    begin_at < DateTime.now && end_at > DateTime.now
+  end
+
+  def already_available
+    !Purchase.where(user_id: self.user_id, purchasable_type: self.purchasable_type, purchasable_id: self.purchasable_id).last.try(:alive)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
   has_many :movies, through: :purchases, source: :purchasable, source_type: :Movie
   has_many :seasons, through: :purchases, source: :purchasable, source_type: :Season
   validates_presence_of :email
-
-  scope :library, -> (user_id) {
-    joins("JOIN purchases ON purchases.user_id = #{user_id}")
-      .joins("JOIN movies ON movies.id = purchases.purchasable_id AND purchases.purchasable_type = 'Movie'")
-      .joins("JOIN seasons ON seasons.id = purchases.purchasable_id AND purchases.purchasable_type = 'Season'")
-      .select('movies.title, seasons.title')
-      .where("purchases.begin_at < :time and purchases.end_at > :time", time: DateTime.now)
-  }
 end
